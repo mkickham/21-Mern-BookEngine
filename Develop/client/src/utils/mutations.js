aa1a5fcb9870ec448fd42efc553110f0cd7caada
Michael Kickham <mfkickham3@gmail.com>
2024-03-15 09:44:36 -0500

    added missing mutations and queries files

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,69 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password){
+            token
+            user {
+                _id
+                username
+            }
+        }
+    }`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+                bookCount
+                savedBooks {
+                   authors
+                    bookId
+                    image
+                    link
+                    title
+                    description
+                  }
+                }
+            }
+        }`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: BookInput!) {
+        saveBook(input: $input) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                title
+                description
+                authors
+                image
+                link
+            }
+        }
+    }`;
+
+export const DELETE_BOOK = gql`
+    mutation deleteBook($userId: ID!, $bookId: String!) {
+        deleteBook(userId: $userId, bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks{
+                bookId
+                title
+                description
+                authors
+                image
+                link
+            }
+        }
+    }`;
---
+++ Develop/client/src/utils/queries.js
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+    query me ($_id: ID!){
+        me(userId: $_id){
+            _id
+            username
+            email
+            bookCount
+            savedBooks{
+                bookId
+                title
+                description
+                authors
+                image
+                link
+            }
+        }
+    }`;
